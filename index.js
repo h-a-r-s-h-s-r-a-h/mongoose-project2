103e7f3fc9a4919c23c41e30413d5987b1ce9dd2
Harsh Choudhary <harshvirat894@gmail.com>
2023-09-18 15:01:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import Blog from "./model/Blog.js";
 import Login from "./model/Login.js";
 
-mongoose.connect("mongodb+srv://harsh:harsh@harsh.21iwvpb.mongodb.net/myDatabase?retryWrites=true&w=majority");
+mongoose.connect(url);
 
 // finding by id with filters
 const article = await Blog.findById("65081320ef6a5f77384be86d","title slug content").exec();
